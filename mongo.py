3e67e359eb3ec88b2b3bb7f3e95cb81d44bf156a
Sam Laubscher <hakaewt@protonmail.com>
2020-09-21 19:20:12 +0000

    Finished section of module

---
+++ mongo.py
@@ -0,0 +1,30 @@
+import os 
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailiure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+coll.update_one({"nationality": "british"}, {"$set": {"nationality": "american"}})
+
+documents = coll.find({"nationality": "american"})
+
+for doc in documents:
+    print(doc)
